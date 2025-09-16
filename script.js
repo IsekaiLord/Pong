8ee24e383023b7b1d086e7b71f8ea27403e4a332
Sisekai <IsekaiLords@gmail.com>
2025-09-16 12:26:52 +0200

    news

---
+++ script.js
@@ -0,0 +1,59 @@
+var uto1 = document.getElementById("uto1");
+var uto2 = document.getElementById("uto2"); 
+var labda = document.getElementById("labda");
+var uto1Y = 300;
+var uto2Y = 300;
+
+var uto1legomb = false;
+var uto1felgomb = false;
+var uto2legomb = false;
+var uto2felgomb = false;
+var labdaXSeb = 0;
+var labdaYSeb = 0;
+var labdaX = 0;
+var labdaY = 0;
+
+uto1.style.left = "40px";
+uto2.style.left = "750px";
+
+document,addEventListener("keydown", function(event){
+    if(event.key == "s") uto1legomb = true;
+    if(event.key == "w") uto1felgomb = true;
+    if(event.key == "ArrowDown") uto2legomb = true;
+    if(event.key == "ArrowUp") uto2felgomb = true;
+});
+
+document,addEventListener("keyup", function(event){
+    if(event.key == "s") uto1legomb = false;
+    if(event.key == "w") uto1felgomb = false;
+    if(event.key == "ArrowDown") uto2legomb = false;
+    if(event.key == "ArrowUp") uto2felgomb = false;
+}); 
+
+if (labdaY < 0 || labdaY > 600) {labdaYSeb = -labdaYSeb};
+if (labdaX < -50) {startLabda();}
+if (labdaX > 750) {startLabda();}
+
+
+function loop(){
+    if(uto1legomb == true) uto1Y += 5;
+    if(uto1felgomb == true) uto1Y -= 5;
+    if(uto2legomb == true) uto2Y += 5;
+    if(uto2felgomb == true) uto2Y -= 5; 
+
+    labdaX += labdaXSeb;
+    labdaY += labdaYSeb;    
+
+    uto1.style.top = utoY-50 + "px";
+    uto2.style.top = uto2Y-50 + "px";
+    labda.style.left = labdaX - 5 + 50 + "px";
+    labda.style.top = labdaY - 5 + "px";
+    window.requestAnimationFrame(loop);
+}
+
+function startLabda(){
+    labdaX = 350;
+    labdaY = 300;
+    labdaXSeb = (Math.random()*3+2) * (Math.round(Math.random()) * 2 - 1);
+    labdaYSeb = (Math.random()*3+2) * (Math.round(Math.random()) * 2 - 1);
+}
